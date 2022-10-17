902ce91485ac3a871aa735d63f8a4509cc971b48
Automatic Linting <data@altamontco.com>
2022-10-16 23:37:39 -0500

    docs: added notes on how to test

--- tests/test_cubes.py
+++ tests/test_cubes.py
@@ -20,8 +20,24 @@ def test_size():
 def test_export():
     """
     Tests Export
+
+    ```
+    >>> c = trimesh.load_mesh("toy.stl")
+    >>> c
+    <trimesh.Trimesh(vertices.shape=(16, 3), faces.shape=(24, 3))>
+    >>> c.bounding_box
+    <trimesh.primitives.Box>
+    >>> c.bounding_box()
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in <module>
+    TypeError: 'Box' object is not callable
+    >>> c.bounds
+    array([[-0.5, -2. , -2.5],
+        [ 4.5,  2. ,  2.5]])
+        ```
+
     """
-    
+
 
 def test_cut():
     """
