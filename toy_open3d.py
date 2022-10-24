cb54c7bc4feafa640d524a1c700f281b214d014c
Automatic Linting <data@altamontco.com>
2022-10-23 22:53:32 -0500

    feat: wip - working on the display system

--- toy_open3d.py
+++ toy_open3d.py
@@ -22,16 +22,28 @@ pcd = o3d.t.geometry.PointCloud(t)
 """
 
 pcd = o3d.geometry.PointCloud()
-pcd_arr = np.array([[x,y,int(random.random()*5000)] for x in range(0,5000) for y in range(0,5000)], dtype=np.int16) 
+
+
+# pcd_arr = np.array([[x,y,int(random.random()*5000)] for x in range(0,5000) for y in range(0,5000)], dtype=np.int16) 
+
+arr = []
+with open("toy.block", "r") as f:
+    for x, line in enumerate(f.readlines()):
+        for y, item in enumerate(line.split(" ")):
+            if item != "\n" and item != "":
+                arr.append([x,y,int(item)])
+
+
+pcd_arr = np.array(arr, dtype=np.int16)
 
 start = time.perf_counter()
 
-pcd_arr[(pcd_arr[:,0] > 2500) & (pcd_arr[:,0] < 3500) & (pcd_arr[:,1] > 3000) & (pcd_arr[:,1] < 5000),2] -= 100
+# pcd_arr[(pcd_arr[:,0] > 2500) & (pcd_arr[:,0] < 3500) & (pcd_arr[:,1] > 3000) & (pcd_arr[:,1] < 5000),2] -= 100
 
 print("diff", time.perf_counter() - start)
 
 print(pcd_arr)
-print(pcd_arr[2500:3500])
+# print(pcd_arr[2500:3500])
 
 pcd.points = o3d.utility.Vector3dVector(pcd_arr)
 
