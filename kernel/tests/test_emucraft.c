d7618ec3b5c9b226c6941761d2c55ac31653c53c
Chester Enright <root@scratch-compiler.chesterenright.com>
2022-10-24 02:20:24 +0000

    test: wip - tests

--- kernel/tests/test_emucraft.c
+++ kernel/tests/test_emucraft.c
@@ -3,15 +3,6 @@
 #include <stdio.h>
 
 
-void test_create_structure(){
-	// Tests creating the structure
-}
-
-void test_freeing_structure(){
-	// Tests tearing down the structure
-}
-
-
 // Variables
 int DIM_X;
 int DIM_Y;
@@ -26,18 +17,43 @@ int CUTTER_Y;
 int* BLOCK;
 
 
-void setup(){
-	// Sets up the variables
-	DIM_X = 1000;
-	DIM_Y = 1000;
+// Sets up the variables
+DIM_X = 10;
+DIM_Y = 10;
 
-	HEIGHT = 1000; // Max is Int16, which is ~30k or 30" in the real world
+HEIGHT = 1000; // Max is Int16, which is ~30k or 30" in the real world
+
+CUTTER_DIAMETER = 6;  	
+CUTTER_HEIGHT = 750;		
+
+CUTTER_X = 5;
+CUTTER_Y = 5;
+
+
+int check_correct_cut(int x,int y){
+	int temp_x;
+	int temp_y;
+
+	if(x < 0){
+		return -1;
+	}else if(x > DIM_X){
+		return -1;
+	}else{
+		temp_x = x;
+	}
 	
-	CUTTER_DIAMETER = 500;  	// .500"
-	CUTTER_HEIGHT = 750;		// .750"
+	if(y < 0){
+		return -2;
+	}else if(y > DIM_Y){
+		return -2;
+	}else{
+		temp_y = y;
+	}
+	printf("[check_correct_cut] Value of (%d, %d) is %d\n", x, y, BLOCK[temp_x * DIM_X + temp_y]);
 	
-	CUTTER_X = 100;
-	CUTTER_Y = 100;
+	return BLOCK[temp_x * DIM_X + temp_y];
+}
+int setup(){
 
 	BLOCK = malloc ((DIM_X * DIM_Y) * sizeof(int));
 	for (int i=0; i<DIM_X * DIM_Y; i++){
@@ -45,20 +61,20 @@ void setup(){
 
 	}
 	// Print 10 x 10 block
-	print_block(BLOCK, 10, 10);
+	print_block(BLOCK, DIM_X, DIM_Y, 0,0, 10);
+
+	if(check_correct_cut(5,5) != 1000){
+		printf("ERROR - block not initialized properly: %d\n", check_correct_cut(5,5));
+		return 1;
+	}
+	return 0;
 }
-int check_correct_cut(int x,int y){
-	return BLOCK[x * DIM_X + y];
-}
+
 int test_cut(){
 	// Tests cutting the structure (removing a circle)
 	
 	printf("Starting up test_cut...");
 
-	if(check_correct_cut(50,50) != 1000){
-		printf("ERROR - block not initialized properly: %d\n", check_correct_cut(50,50));
-		return 1;
-	}
 	
 	int success = cut(BLOCK, CUTTER_X, CUTTER_Y, CUTTER_DIAMETER, CUTTER_HEIGHT, DIM_X, DIM_Y);
 	if(success != 0){
@@ -66,60 +82,64 @@ int test_cut(){
 		return 1;
 
 	}
-	if(BLOCK[50 * DIM_X + 50] != 750){
-		printf("ERROR - block not cut to right height.");
-		return 1;
-	}
-	
 	return 0;
-
 }
 
 
 
 int test_correct_circle_boundary(){
 	// Test that only within the circle is cut
-
-	if(check_correct_cut(50,50) != 1000){
+	if(check_correct_cut(5,5) != 1000){
 		printf("ERROR - block not initialized properly.");
 		return 1;
 	}
 
+
 	cut(BLOCK, CUTTER_X, CUTTER_Y, CUTTER_DIAMETER, CUTTER_HEIGHT, DIM_X, DIM_Y);
+
 	
 	int x = 0;
 	int y = 0;
 
 
-	// Left Boundary
-	x = CUTTER_X - ceil(CUTTER_DIAMETER/2);
+	// Left Boundary 
+	x = CUTTER_X - ceil(CUTTER_DIAMETER/2) + 1;
 	y = CUTTER_Y;
-	
+
+
 	if(check_correct_cut(x,y) != CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary not the right height: %d\n", BLOCK[x * DIM_X + y]);
+		printf("ERROR - Top Left Boundary (%d, %d) not the right height: %d\n", x, y, check_correct_cut(x,y));
+		print_block(BLOCK, DIM_X, DIM_Y, 0, 0, 10);
 		return 1;
 	}
-	if(check_correct_cut(x,y+1) == CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary + 1 is at the wrong height: %d\n", check_correct_cut(x,y+1));
+
+
+	if(check_correct_cut(x,y+3) == CUTTER_HEIGHT){
+		printf("ERROR - Top Left Boundary + 3 (%d, %d) is at the wrong height: %d\n", x, y+3, check_correct_cut(x,y+3));
+		
+		printf("Block:%d, %d\n", x, y);
+		print_block(BLOCK, DIM_X, DIM_Y, 0, 0, 10);
 		return 1;
 
 	}
-	if(check_correct_cut(x+1, y) == CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary + 1 is at the wrong height: %d\n", check_correct_cut(x+1, y));
+	if(check_correct_cut(x-1, y) == CUTTER_HEIGHT){
+		printf("ERROR - Top Left Boundary - 1 (%d, %d) is at the wrong height: %d\n", x-1, y, check_correct_cut(x-1, y));
+		print_block(BLOCK, DIM_X, DIM_Y, 0, 0, 10);
 		return 1;
 
 	}
 	
 	// Right Boundary
-	x = CUTTER_X + ceil(CUTTER_DIAMETER/2);
+	x = CUTTER_X + ceil(CUTTER_DIAMETER/2) - 1;
 	y = CUTTER_Y;
 
 	if(check_correct_cut(x,y) != CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary not the right height: %d\n", BLOCK[x * DIM_X + y]);
+		printf("ERROR - Top Right Boundary (%d, %d) not the right height: %d\n", x, y, BLOCK[x * DIM_X + y]);
 		return 1;
 	}
-	if(check_correct_cut(x,y+1) == CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary + 1 is at the wrong height: %d\n", check_correct_cut(x,y+1));
+	if(check_correct_cut(x,y+3) == CUTTER_HEIGHT){
+		printf("ERROR - Top Right Boundary + 3 is at the wrong height: %d\n", check_correct_cut(x,y+3));
+		print_block(BLOCK, DIM_X, DIM_Y, 0, 0, 10);
 		return 1;
 
 	}
@@ -131,30 +151,33 @@ int test_correct_circle_boundary(){
 	
 
 	// Top Boundary
-	y = CUTTER_Y + ceil(CUTTER_DIAMETER/2);
+	y = CUTTER_Y + ceil(CUTTER_DIAMETER/2) - 1;
 	x = CUTTER_X;
 
 	if(check_correct_cut(x,y) != CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary not the right height: %d\n", BLOCK[x * DIM_X + y]);
+		printf("ERROR - Top Boundary (%d, %d) not the right height: %d\n", x, y, BLOCK[x * DIM_X + y]);
+		print_block(BLOCK, DIM_X, DIM_Y, 0, 0, 10);
 		return 1;
 	}
 	if(check_correct_cut(x,y+1) == CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary + 1 is at the wrong height: %d\n", check_correct_cut(x,y+1));
+		printf("ERROR - Top Boundary + 1 is at the wrong height: %d\n", check_correct_cut(x,y+1));
 		return 1;
 
 	}
-	if(check_correct_cut(x+1, y) == CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary + 1 is at the wrong height: %d\n", check_correct_cut(x+1, y));
+	if(check_correct_cut(x+3, y) == CUTTER_HEIGHT){
+		printf("ERROR - Top Boundary (%d, %d) + 3 is at the wrong height: %d\n", x+3, y, check_correct_cut(x+3, y));
+		print_block(BLOCK, DIM_X, DIM_Y, 0, 0, 10);
 		return 1;
 
 	}
 
 	// Bottom Boundary
 	x = CUTTER_X;
-	y = CUTTER_Y + ceil(CUTTER_DIAMETER/2);
+	y = CUTTER_Y + ceil(CUTTER_DIAMETER/2) - 1;
 
 	if(check_correct_cut(x,y) != CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary not the right height: %d\n", BLOCK[x * DIM_X + y]);
+		printf("ERROR - Bottom Boundary (%d, %d) not the right height: %d\n", x,y, BLOCK[x * DIM_X + y]);
+		print_block(BLOCK, DIM_X, DIM_Y, 0, 0, 10);
 		return 1;
 	}
 	if(check_correct_cut(x,y+1) == CUTTER_HEIGHT){
@@ -162,48 +185,34 @@ int test_correct_circle_boundary(){
 		return 1;
 
 	}
-	if(check_correct_cut(x+1, y) == CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary + 1 is at the wrong height: %d\n", check_correct_cut(x+1, y));
-		return 1;
+	if(check_correct_cut(x+3, y) == CUTTER_HEIGHT){
+		printf("ERROR - Top Left Boundary + 3 is at the wrong height: %d\n", check_correct_cut(x+3, y));
+		return 3;
 
 	}
 	
 	// Point on circle x = r * sin theta, y = r * cos theta
-	int angle = PI/180 * 45;
-	x = ceil(sin(angle) * CUTTER_DIAMETER/2);
-	y = ceil(cos(angle) * CUTTER_DIAMETER/2);
-	
-	if(check_correct_cut(x,y) != CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary not the right height: %d\n", BLOCK[x * DIM_X + y]);
-		return 1;
-	}
-	if(check_correct_cut(x,y+1) == CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary + 1 is at the wrong height: %d\n", check_correct_cut(x,y+1));
-		return 1;
+	for(float angle = 0; angle < PI/180 * 360; angle += PI/180 * 45){
+		printf("Testing angle %f  ...\n", angle / (PI/180));
 
-	}
-	if(check_correct_cut(x+1, y) == CUTTER_HEIGHT){
-		printf("ERROR - Top Left Boundary + 1 is at the wrong height: %d\n", check_correct_cut(x+1, y));
-		return 1;
-
+		x = CUTTER_X + ceil(sin(angle) * CUTTER_DIAMETER/2) - 1;
+		y = CUTTER_Y + ceil(cos(angle) * CUTTER_DIAMETER/2) - 1;
+		
+		if(check_correct_cut(x,y) != CUTTER_HEIGHT){
+			printf("ERROR - Angle Boundary (%d, %d) not the right height: %d\n", x, y, BLOCK[x * DIM_X + y]);
+			print_block(BLOCK, DIM_X, DIM_Y, 0, 0, 10);
+			return 1;
+		}
+		
 	}
 
 	
-	// Test to make sure that a value is returned when the block has been cut
-	// Test to make sure a different value is returned when the block has not
+	// TODO: Test to make sure that a value is returned when the block has been cut
+	// TODO: Test to make sure a different value is returned when the block has not
 
-	// Test to make sure the difference (delta) is returned somewhere
+	// TODO: Test to make sure the difference (delta) is returned somewhere
 	return 0;
 }
-
-int test_read_in_file(){
-	// Tests reading in a processed file (not G-code, the XYZ file plus cutting information)
-
-	// What if the file is bad formatting?
-	return 0;
-}
-
-
 
 int main(){
 	printf("Starting setup...\n");
@@ -216,22 +225,51 @@ int main(){
 	int output = 0;
 
 	// Test at Normal height
-
+	
 	output = test_cut();
-
+	if(BLOCK[5 * DIM_X + 5] != CUTTER_HEIGHT){
+		printf("ERROR - block not cut to right height.:%d\n", BLOCK[5 * DIM_X + 5]);
+		return 1;
+	}
 	printf("Output of test_cut: %d\n", output);
 	if(output != 0){
 		return output;
 	}
-	
+
+	setup();
 
 	// Test recutting (or cutting at lower height)
+
+	CUTTER_HEIGHT = 1100;
+
 	test_cut();
+	if(BLOCK[50 * DIM_X + 50] == CUTTER_HEIGHT){
+		printf("ERROR [Recut] - block not cut to right height.:%d\n", BLOCK[50 * DIM_X + 50]);
+		return 1;
+	}
 	printf("Output of test_cut: %d\n", output);
 	if(output != 0){
 		return output;
 	}
 	
+	// Test correct circle boundary
+	DIM_X = 1000;
+	DIM_Y = 1000;
+	CUTTER_DIAMETER = 100;  	
+	CUTTER_HEIGHT = 750;		
+
+	CUTTER_X = 200;
+	CUTTER_Y = 200;
+
+	setup();
+
+	printf("Testing the circle removal...\n");
+	output = test_correct_circle_boundary();
+	if(output != 0){
+		printf("Test failed: %d\n", output);
+		return output;
+	}
+	printf("Output of test correct circle boundary: %d\n", output);
 	return 0;
 }
 
---
+++ kernel/tests/test_read_file.c
@@ -0,0 +1,12 @@
+#include "../include/functions.h"
+#include <math.h>
+#include <stdio.h>
+
+int test_read_in_file(){
+	// Tests reading in a processed file (not G-code, the XYZ file plus cutting information)
+
+	// What if the file is bad formatting?
+	return 0;
+}
+
+
