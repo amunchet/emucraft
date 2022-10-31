2c75e556bd945a753560fa23902f6875f562e54b
Automatic Linting <data@altamontco.com>
2022-10-31 01:50:08 -0500

    debug: note on lines in queue

--- kernel/include/functions.h
+++ kernel/include/functions.h
@@ -228,6 +228,7 @@ int process_from_file(int *BLOCK, int DIM_X, int DIM_Y, char *filename)
 	}
 
 	// Clean up any extra delta lines
+	printf("We have %d extra lines in queue\n", line_count);
 	for (int i = 0; i < line_count; i++)
 	{
 		fprintf(delta_output, "%s", delta_lines[line_count]);
