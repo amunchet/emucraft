67381c53ac235c98612403862bd005b528493ec5
Automatic Linting <data@altamontco.com>
2022-10-30 23:24:22 -0500

    chore: linting

--- kernel/include/functions.h
+++ kernel/include/functions.h
@@ -4,133 +4,156 @@
 
 #define PI 3.14159265
 
-
-void print_block(int* BLOCK, int DIM_X, int DIM_Y, int start_x, int start_y, int size){
+void print_block(int *BLOCK, int DIM_X, int DIM_Y, int start_x, int start_y, int size)
+{
 	printf("    |\t");
-	for(int y=start_y; y<start_y+size; y++){
+	for (int y = start_y; y < start_y + size; y++)
+	{
 		printf("%d\t", y);
 	}
 	printf("\n");
 
-	for(int x = start_x; x<start_x+size; x++){
-		for(int y = start_y; y<start_y+size; y++){
-			if(y == start_y){
+	for (int x = start_x; x < start_x + size; x++)
+	{
+		for (int y = start_y; y < start_y + size; y++)
+		{
+			if (y == start_y)
+			{
 				printf("%d |\t", x);
 			}
-			if(y < DIM_Y && x < DIM_X && x >= 0 && y >= 0){
+			if (y < DIM_Y && x < DIM_X && x >= 0 && y >= 0)
+			{
 				printf(" %d\t", BLOCK[x * DIM_X + y]);
 			}
 		}
-		
+
 		printf("\n");
 	}
 }
-int check_distance(int x, int y, int CUTTER_X, int CUTTER_Y, int CUTTER_DIAMETER){
+int check_distance(int x, int y, int CUTTER_X, int CUTTER_Y, int CUTTER_DIAMETER)
+{
 	int distance_x = abs(x - CUTTER_X);
 	int distance_y = abs(y - CUTTER_Y);
 
-	float value = sqrt((distance_x * distance_x) + (distance_y * distance_y)); 
-	float radius = CUTTER_DIAMETER/2;
+	float value = sqrt((distance_x * distance_x) + (distance_y * distance_y));
+	float radius = CUTTER_DIAMETER / 2;
 
-	//printf("[check_distance] (%d, %d) with cutter (%d, %d) (d: %d) = %d < %d\n", x, y, CUTTER_X, CUTTER_Y, CUTTER_DIAMETER, value , radius);
+	// printf("[check_distance] (%d, %d) with cutter (%d, %d) (d: %d) = %d < %d\n", x, y, CUTTER_X, CUTTER_Y, CUTTER_DIAMETER, value , radius);
 
-	if(value < radius) {
-		return 1;	
+	if (value < radius)
+	{
+		return 1;
 	}
 	return 0;
 }
 
-int cut(int* BLOCK, int CUTTER_X, int CUTTER_Y, int CUTTER_DIAMETER, int CUTTER_HEIGHT, int BLOCK_X, int BLOCK_Y){
-	
+int cut(int *BLOCK, int CUTTER_X, int CUTTER_Y, int CUTTER_DIAMETER, int CUTTER_HEIGHT, int BLOCK_X, int BLOCK_Y)
+{
+
 	// Find a block around center of cutter
-	
+
 	int min_x = 0;
 	int max_x = 0;
-	
+
 	int min_y = 0;
 	int max_y = 0;
 
-	min_x = floor(CUTTER_X - (CUTTER_DIAMETER/2));
-	if (min_x < 0){
+	min_x = floor(CUTTER_X - (CUTTER_DIAMETER / 2));
+	if (min_x < 0)
+	{
 		min_x = 0;
 	}
-	if (min_x > BLOCK_X){
+	if (min_x > BLOCK_X)
+	{
 		min_x = BLOCK_X;
 	}
-	
-	max_x = ceil(CUTTER_X + (CUTTER_DIAMETER/2));
-	if (max_x > BLOCK_X){
+
+	max_x = ceil(CUTTER_X + (CUTTER_DIAMETER / 2));
+	if (max_x > BLOCK_X)
+	{
 		max_x = BLOCK_X;
 	}
-	if(max_x < 0){
+	if (max_x < 0)
+	{
 		max_x = 0;
 	}
 
-	min_y = floor(CUTTER_Y - (CUTTER_DIAMETER/2));
-	if(min_y < 0){
+	min_y = floor(CUTTER_Y - (CUTTER_DIAMETER / 2));
+	if (min_y < 0)
+	{
 		min_y = 0;
 	}
-	if(min_y > BLOCK_Y){
+	if (min_y > BLOCK_Y)
+	{
 		min_y = BLOCK_Y;
 	}
-	
-	max_y = ceil(CUTTER_Y + (CUTTER_DIAMETER/2));
-	if(max_y < 0){
+
+	max_y = ceil(CUTTER_Y + (CUTTER_DIAMETER / 2));
+	if (max_y < 0)
+	{
 		max_y = 0;
 	}
-	if(max_y > BLOCK_Y){
+	if (max_y > BLOCK_Y)
+	{
 		max_y = BLOCK_Y;
 	}
-	
+
 	// Check for edges
-	
+
 	int total_removed = 0; // This is .001 cubic inches
 	int cube_count = 0;
 
 	int difference = 0;
 
-	for(int x = min_x; x<max_x; x++){
-		for(int y = min_y; y<max_y; y++){
-			if(check_distance(x, y, CUTTER_X, CUTTER_Y, CUTTER_DIAMETER) && BLOCK[x * BLOCK_X + y] > CUTTER_HEIGHT){
+	for (int x = min_x; x < max_x; x++)
+	{
+		for (int y = min_y; y < max_y; y++)
+		{
+			if (check_distance(x, y, CUTTER_X, CUTTER_Y, CUTTER_DIAMETER) && BLOCK[x * BLOCK_X + y] > CUTTER_HEIGHT)
+			{
 				difference = BLOCK[x * BLOCK_X + y] - CUTTER_HEIGHT;
-				//printf("[Cut] Difference: %d\n", difference);
+				// printf("[Cut] Difference: %d\n", difference);
 				total_removed += difference;
-				BLOCK[x * BLOCK_X +y] = CUTTER_HEIGHT;	
+				BLOCK[x * BLOCK_X + y] = CUTTER_HEIGHT;
 				cube_count += 1;
-			}	
+			}
 		}
 	}
-	
-	//printf("[Cut] Removed %d number of cubes.\n", cube_count);
+
+	// printf("[Cut] Removed %d number of cubes.\n", cube_count);
 	return total_removed;
 }
 
-int write_block(int* BLOCK, int DIM_X, int DIM_Y, char* filename){
+int write_block(int *BLOCK, int DIM_X, int DIM_Y, char *filename)
+{
 	printf("Writing to %s\n", filename);
 	FILE *fp = fopen(filename, "w+");
-	for(int x = 0; x<DIM_X; x++){
-		for(int y = 0; y<DIM_Y; y++){
+	for (int x = 0; x < DIM_X; x++)
+	{
+		for (int y = 0; y < DIM_Y; y++)
+		{
 			fprintf(fp, "%d ", BLOCK[x * DIM_X + y]);
 		}
-		fprintf(fp,"\n");
+		fprintf(fp, "\n");
 	}
 	fclose(fp);
 	return 0;
 }
 
-int process_from_file(int* BLOCK, int DIM_X, int DIM_Y, char* filename){
+int process_from_file(int *BLOCK, int DIM_X, int DIM_Y, char *filename)
+{
 	/*
 	Processes a XYZ File
-	
+
 	Format:
 		[X] [Y] [Z] [Cutter Diameter] [Cutter Z value] [Tool Holder Diameter] [Tool Holder Z (Bottom)]
 	*/
 
 	FILE *fp = fopen(filename, "r");
 	int x, y, z, cutter_diameter, cutter_z;
-	
+
 	fscanf(fp, "%d %d %d %d %d\n", &x, &y, &z, &cutter_diameter, &cutter_z);
-	
-	//cut(BLOCK, CUTTER_X, CUTTER_Y, CUTTER_DIAMETER, CUTTER_HEIGHT, DIM_X, DIM_Y);
+
+	// cut(BLOCK, CUTTER_X, CUTTER_Y, CUTTER_DIAMETER, CUTTER_HEIGHT, DIM_X, DIM_Y);
 	return 1;
 }
