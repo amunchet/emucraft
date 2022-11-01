9653c6dd96006720aa1c00dae834c1c918862c9e
Automatic Linting <data@altamontco.com>
2022-11-01 00:26:39 -0500

    feat: wip - added helper functions

--- toy_open3d.py
+++ toy_open3d.py
@@ -8,6 +8,10 @@ import time
 
 dim_x = 1000
 dim_y = 1000
+
+special_run = False
+z_point_cloud = None
+
 # Need to add Z points for the visual side walls
 def add_z_points(block, start, diff, block_x, block_y):
     """
@@ -39,10 +43,6 @@ def data():
                 if item != "\n" and item != "":
                     arr.append([x,y,int(item)])
                     
-
-
-    
-    
     print("Done")
 
     pcd_arr = np.array(arr, dtype=np.int16)
@@ -52,8 +52,43 @@ def data():
     return pcd
 
 
-special_run = False
-z_point_cloud = None
+def check_if_needs_z(pcd_points, dim_x, x, y):
+    """
+    Checks if a given point needs a Z array generation (for visualization)
+    - `pcd_points` - array of points
+
+    - Only looks behind to avoid duplicates
+    """
+    temp_arr = []
+    current = pcd_points[x * dim_x + y]
+    prev_x = pcd_points[(x - 1) * dim_x + y]
+    prev_y = pcd_points[x * dim_x + (y-1)]
+
+    # Check for X 
+
+
+    # Check for Y
+    if current[2] != prev_x[2]:
+        output = add_z_points(None, (x, y, current), (x-1, y, prev_x), dim_x, None)
+        temp_arr += output
+    
+    if current[2] != prev_y[2]:
+        output = add_z_points(None, (x, y, current), (x, y-1, prev_y), dim_x, None)
+        temp_arr += output
+
+    # Return list of point groups
+    return temp_arr
+
+
+
+def parse_z_lookup(z_lookup):
+    """
+    Returns Z lookup as a workable array
+    """
+    retval = []
+    for value in z_lookup.itervalues():
+        retval += value
+    return retval
 
 def custom_draw_geometry_load_option(pcd):
     vis = o3d.visualization.VisualizerWithKeyCallback()
@@ -69,6 +104,11 @@ def custom_draw_geometry_load_option(pcd):
 
         special_run = not special_run
 
+
+        z_lookup = {} # Z lookup stores the (x,y) locations and is a list of the Z values
+
+
+
         with open("toy.xyz.sim") as f:
             current_step = 0
             for line in f.readlines():
@@ -78,8 +118,17 @@ def custom_draw_geometry_load_option(pcd):
                 loc = int(x) * int(dim_x) + int(y)
                 pcd_points[loc][2] = z # TODO: HARD SET X SIZE TO 1000
 
+                temp = check_if_needs_z(pcd_points, dim_x, x, y) # Only looks behind to avoid duplicates
+                if temp: # Returns none if it doesn't need it
+                    print("Found a temp (z-wall):", temp)
+                    z_lookup[(x,y)] = temp
 
                 if step != current_step:
+                    z_point_cloud.points = o3d.utility.Vector3dVector(
+                        np.array(parse_z_lookup(z_lookup), dtype=np.int16)
+                    )
+
+                    vis.update_geometry(z_point_cloud)
                     vis.update_geometry(pcd)
                     vis.poll_events()
                     vis.update_renderer()
@@ -87,7 +136,8 @@ def custom_draw_geometry_load_option(pcd):
                     current_step = step
 
         # I know the block size
-        # TODO - This is currently broken
+
+        # TODO: Remove all this code
         print("Starting to add Z points...")
         temp_arr = []
         arr = pcd_points
