0215bfc95da41639b091bd2ad41dbd99b27760f6
Chester Enright <amunchet@gmail.com>
2023-03-11 02:21:10 -0600

    feat: wip - working parse_comments

--- gcode/gcode_parser.py
+++ gcode/gcode_parser.py
@@ -3,6 +3,7 @@
 G-code parser
 """
 import re
+from gcode_logger import logger
 
 # Exceptions
 class NotImplementedException(Exception):
@@ -23,9 +24,9 @@ def codes_parse(gcode):
     for line in lines:
         line_codes = []
         # Split the line into tokens
-        print(line)
-        tokens = re.findall(r"[A-Z]\d*\.?\d+", line)
-        print(tokens)
+        logger.debug(line)
+        tokens = re.findall(r"[A-Z]-?\d*\.?\d+", line)
+        logger.debug(tokens)
         # Extract the code and value from the line
 
         for token in tokens:
@@ -39,7 +40,7 @@ def codes_parse(gcode):
         
         codes.append(line_codes)
 
-    print("Codes:", codes)
+    logger.debug(f"Codes:{codes}")
     return codes
 
 class Program:
@@ -59,29 +60,68 @@ class Program:
 
         self.starting_point = (0,0,10) # Default
 
+        self.block_x_min = None
+        self.block_x_max = None
+
+        self.block_y_min = None
+        self.block_y_max = None
+
+        self.block_z_min = None
+        self.block_z_max = None
+
+    def helper_block(self):
+        """
+        Turns Block Values into one data structure for ease of use
+
+        Also calculates the block_offsets
+        """
+        logger.debug("In helper block")
+
+        self.block = {
+            "x" : (self.block_x_min, self.block_x_max),
+            "y" : (self.block_y_min, self.block_y_max),
+            "z" : (self.block_z_min, self.block_z_max),
+        }
+        self.block_offsets = {
+            "x" : abs(self.block_x_min),
+            "y" : abs(self.block_y_min),
+            "z" : 0
+        }
 
     def parse_line(self, lines):
         """
         Parses Lines
         """
-        print("Lines:", lines)
+        logger.debug(f"Lines:{lines}")
         # Check if the right types, if not, send through codes_parse
         # If that fails, throw an error
 
+        number_match = r"(-?(\d+.?)\d+(\.\d+)?)"
+
         matches = {
-            r"Tool Holder Diameter: ((\d+.?)\d+(\.\d+)?)" : "tool_holder_diameter",
-            r"#127\s*=\s*(\d+(\.\d+)?)" : "tool_holder_length",
-            r"DIAMETER:\s*(\d+(\.\d+)?)" : "tool_diameter",
-            r"#128\s*=\s*(\d+(\.\d+)?)" : "tool_length",
+            r"Tool Holder Diameter: {}" : "tool_holder_diameter",
+            r"#127\s*=\s*{}" : "tool_holder_length",
+            r"DIAMETER:\s*{}" : "tool_diameter",
+            r"#128\s*=\s*{}" : "tool_length",
+            r"MIN\s*X\s*:\s*{}" : "block_x_min",
+            r"MAX\s*X\s*:\s*{}" : "block_x_max",
+
+            r"MIN\s*Y\s*:\s*{}" : "block_y_min",
+            r"MAX\s*Y\s*:\s*{}" : "block_y_max",
+
+            r"MIN\s*Z\s*:\s*{}" : "block_z_min",
+            r"MAX\s*Z\s*:\s*{}" : "block_z_max",
+
+
         }
 
         for line in [x.strip() for x in lines.split("\n") if x.strip() != ""]:
-            print("Line:", line)
+            logger.debug(f"Line:{line}")
 
             for match in matches:
-                x = re.search(match, line)
+                x = re.search(match.format(number_match), line)
                 if x is not None:
-                    print(x.group(1))
+                    logger.warning(x.group(1))
 
                     try:
                         output = float(x.group(1))
@@ -89,5 +129,10 @@ class Program:
                         output = x.group(1)
 
                     setattr(self, matches[match], output)
+        
+        # Run all helper functions
+
+        for item in [x for x in dir(self) if x.startswith("helper_")]:
+            getattr(self, item)()
         
         return True
