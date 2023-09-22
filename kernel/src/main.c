9efc0ea8ba002f566b635cada2568b60c3055a74
Chester Enright <amunchet@gmail.com>
2023-09-22 08:50:17 -1000

    chore: wip - starting on cffi

---
+++ kernel/src/main.c
@@ -0,0 +1,79 @@
+#include "../include/functions.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Variables
+int DIM_X;
+int DIM_Y;
+
+int HEIGHT; // Max is Int16, which is ~30k or 30" in the real world
+
+int CUTTER_DIAMETER;  	
+int CUTTER_HEIGHT;
+int CUTTER_X;
+int CUTTER_Y;
+
+int* BLOCK;
+
+
+// Sets up the variables
+DIM_X = 5000;
+DIM_Y = 5000;
+
+HEIGHT = 1000; // Max is Int16, which is ~30k or 30" in the real world
+
+CUTTER_DIAMETER = 250;  	
+CUTTER_HEIGHT = 750;		
+
+CUTTER_X = 300;
+CUTTER_Y = 300;
+
+
+
+int setup(){
+
+	BLOCK = malloc ((DIM_X * DIM_Y) * sizeof(int));
+	for (int i=0; i<DIM_X * DIM_Y; i++){
+		BLOCK[i] = 2000;
+
+	}
+	// Print 10 x 10 block
+	//print_block(BLOCK, DIM_X, DIM_Y, 0,0, 10);
+
+	return 0;
+}
+
+
+
+
+
+int main(){
+	printf("Starting setup...\n");
+	setup();
+
+	printf("Setup done\n");
+
+	// Tests cutting the structure (removing a circle)
+	
+	printf("Starting up toy cut...");
+
+	int success = write_block(BLOCK, DIM_X, DIM_Y, "toy.block");
+	if(success != 0){
+		printf("ERROR - write failed.");
+		return 1;
+
+	}
+	
+	success = process_from_file(BLOCK, DIM_X, DIM_Y, "toy.xyz");
+
+	
+	
+	
+
+	printf("Done.\n");
+
+	return 0;
+}
+
