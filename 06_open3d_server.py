642b7fae8c25c5eef1c616efa33fe5cf456de0cd
Chester Enright <amunchet@gmail.com>
2023-09-27 00:45:54 -1000

    perf: better use of threads for updating

--- 06_open3d_server.py
+++ 06_open3d_server.py
@@ -1,6 +1,7 @@
 import os
 import time
 import queue
+import threading
 
 import open3d as o3d
 import numpy as np
@@ -64,58 +65,81 @@ w.add_geometry("mesh", mesh)
 
 paused = False
 speed = 10
+updated = True
 
 
 w.reset_camera_to_default()
 
 # Main Loop
-i = 0
+def thread_main():
+    i = 0
+    global paused 
+    global speed
+    global updated
 
-with open("gcode/tests/test.xyz") as f:
-    lines = f.readlines()
+    with open("gcode/tests/test.xyz") as f:
+        lines = f.readlines()
 
-seen_x = None
-seen_y = None
-seen_z = None
-skipping_z = False
-for i,item in enumerate(lines):
-    cur_x,cur_y,cur_z,diameter,tool_diam,tool_length,move_type = [round(int(x)/10) for x in item.split(" ")]
-    while paused:
-        time.sleep(1)
-    if not paused:
-        # print((cur_x, cur_y, cur_z))
-        if cur_x == seen_x and cur_y == seen_y:
-            skipping_z = True
-            continue
+    seen_x = None
+    seen_y = None
+    seen_z = None
+    skipping_z = False
+    for i,item in enumerate(lines):
+        cur_x,cur_y,cur_z,diameter,tool_diam,tool_length,move_type = [round(int(x)/10) for x in item.split(" ")]
+        while paused:
+            time.sleep(1)
+        if not paused:
+            # print((cur_x, cur_y, cur_z))
+            if cur_x == seen_x and cur_y == seen_y:
+                skipping_z = True
+                continue
+            
+            if skipping_z:
+                reduce_z_values(z, (seen_x, seen_y), diameter, seen_z)
+                skipping_z = False
+
+            seen_x = cur_x
+            seen_y = cur_y
+            seen_z = cur_z
+            
+            # print(diameter)
+            reduce_z_values(z, (cur_x, cur_y), diameter, cur_z)
+            if (i % speed == 0):
+                vertices = np.column_stack([xx.ravel(), yy.ravel(), z.ravel()])
+                mesh.vertices = o3d.utility.Vector3dVector(vertices)
+                mesh.compute_vertex_normals()
+            # app.post_to_main_thread(w, update_geometry)
+            # update_geometry()
+            # time.sleep(0.001)
+                updated = True
+                while updated:
+                    time.sleep(0.001)
+
+def update_geometry():
+    global updated
+    if updated:
+
+        w.remove_geometry("frame")
+        w.remove_geometry("mesh")
+
+        w.add_geometry("frame", frame)
+        w.add_geometry("mesh", mesh)
+
+        w.post_redraw()
         
-        if skipping_z:
-            reduce_z_values(z, (seen_x, seen_y), diameter, seen_z)
-            skipping_z = False
 
-        seen_x = cur_x
-        seen_y = cur_y
-        seen_z = cur_z
-        
-        # print(diameter)
-        reduce_z_values(z, (cur_x, cur_y), diameter, cur_z)
-        if (i % speed == 0):
-            vertices = np.column_stack([xx.ravel(), yy.ravel(), z.ravel()])
-            mesh.vertices = o3d.utility.Vector3dVector(vertices)
-            mesh.compute_vertex_normals()
-    
-    w.remove_geometry("frame")
-    w.remove_geometry("mesh")
+        updated = False
 
-    w.add_geometry("frame", frame)
-    w.add_geometry("mesh", mesh)
-
-
-
-    w.post_redraw()
     app.run_one_tick()
-    w.reset_camera_to_default()
     
-    time.sleep(0.001)
-
+# thread_main()
 # Continue to run after the loop is finished
-app.run()
+threading.Thread(target=thread_main).start()
+
+w.show_skybox(False)
+w.set_background((0.1, 0.1, 0.1, 0.5), None)
+w.setup_camera(90, [0,0,0], [500,500,500], [0,0,1])
+while True:
+    update_geometry()
+
+# app.run()
